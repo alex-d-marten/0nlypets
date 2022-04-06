85f5ebf5a195f753983091d759b5f34e717d2a3e
amarten <alexdmarten@gmail.com>
2022-04-05 21:43:14 -0700

    updated Comment, User and Post model

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,10 +1,26 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require('../utils/dateFormat');
 
 const commentSchema = new Schema(
     {
         commentText: {
             type: String,
-            required: 'You need a comment'
+            required: 'You need a comment',
+            minlength: 1,
+            maxLength: 400
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timeStamp => dateFormat(timeStamp)
+        },
+        username: {
+            type: String,
+            required: true
         }
     }
-)
+);
+
+const Comment = model('Comment', commentSchema);
+
+module.exports = Comment;
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require("mongoose");
 
+
 const postSchema = new Schema(
   {
     petName: {
