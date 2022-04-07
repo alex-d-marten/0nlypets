3aba0c23010b241c80f2d923736be2e86a1882c4
naomi <naomilounsbury@hotmail.com>
2022-04-06 19:03:44 -0700

    in the client side, the home page, profile page, single post page, and components postform and postlist

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,1 +1,62 @@
 // This is the user's profile page where they can update their profile and upload pictures of their pets and view a single post if they click on one
+import React from "react";
+import { Redirect, useParams } from "react-router-dom";
+
+import PostForm from "../components/PostForm";
+import PostList from "../components/PostList";
+
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { ADD_POST } from "../utils/mutations";
+import Auth from "../utils/auth";
+
+const Profile = (props) => {
+  const { username: userParam } = useParams();
+
+  const [addPost] = useMutation(ADD_POST);
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
+  });
+
+  const user = data?.me || data?.user || {};
+
+  // redirect to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to="/profile" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  }
+
+  return (
+    <div>
+      <div className="flex-row mb-3">
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
+        </h2>
+      </div>
+
+      <div className="flex-row justify-space-between mb-3">
+        <div className="col-12 mb-3 col-lg-8">
+          <PostList
+            Post={user.posts}
+            title={`${user.username}'s pet posts...`}
+          />
+        </div>
+      </div>
+      <div className="mb-3">{!userParam && <PostForm />}</div>
+    </div>
+  );
+};
+
+export default Profile;
