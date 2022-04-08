f6b9a54e54ce8c2f05b0ff2a322c373416ff0bf3
CloudyLondon <jovialcty@gmail.com>
2022-04-07 18:20:13 -0700

    update utils/mutations and utils/queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,86 @@
 import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_POST = gql`
+  mutation addPost($postText: String!) {
+    addPost(post: $postText) {
+      _id
+      petName
+      caption
+      createdAt
+      username
+      image
+      comments {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($postId: ID!, $commentBody: String!) {
+    addComment(postId: $postId, commentText: $commentText) {
+      _id
+      comments {
+        _id
+        commentText
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+export const REMOVE_POST = gql`
+  mutation removePost($id: ID!) {
+    removePost(id: $id) {
+      _id
+      username
+    }
+  }
+`;
+
+export const REMOVE_USER = gql`
+  mutation removeUser($id: ID!) {
+    removeUser(id: $id) {
+      _id
+      username
+    }
+  }
+`;
+
+export const REMOVE_COMMENT = gql`
+  mutation removeComment($id: ID!) {
+    removeComment(id: $id) {
+      _id
+      username
+      comments {
+        _id
+        username
+      }
+    }
+  }
+`;
