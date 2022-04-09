187dc87f8f08307a099ef30f2b11e7eca095aced
naomi <naomilounsbury@hotmail.com>
2022-04-08 17:49:12 -0700

    adds bootstrap to index.js, add renderpage to app.js, changes the nav to a ternairy, adds apolloprovider to app.js, fixes some bugs in postlist

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,50 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 // component import
-import Header from './components/Header';
-import Footer from './components/Footer';
-import logo from "./logo.svg";
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+//import logo from "./logo.svg";
+const client = new ApolloClient({
+  // link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   // keeps track of which link in the Nav has been clicked
   // and is currently active.
-  const [currentPage, setCurrentPage] = useState('Start');
+  const [currentPage, setCurrentPage] = useState("Home");
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "Profile") {
+      return <Profile />;
+    }
+    if (currentPage === "Signup") {
+      return <Signup />;
+    }
+    if (currentPage === "Login") {
+      return <Login />;
+    }
+    return Home;
+  };
 
   return (
-    <div>
-      <Header
-      currentPage={currentPage}
-      setCurrentPage={setCurrentPage}
-      ></Header>
-      <section className='main-section'>
-
-        {/* Put all pages in between this section tag */}
-
-      </section>
-      <Footer></Footer>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Header
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        ></Header>
+        <section className="main-section">{renderPage()}</section>
+        <Footer></Footer>
+      </div>
+    </ApolloProvider>
   );
 }
 
