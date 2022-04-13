eecac423bd537de31b855e04347030ddc16bdd46
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-12 21:00:03 -0700

    redirecting 0nlyPets title to home when logged in and signup when not logged in

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,17 +1,27 @@
 import Nav from "../Nav";
+import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
+
 
 function Header({ currentPage, setCurrentPage }){
     return(
-        <header className="top-header bg-info bg-opacity-25">
+        <header className="top-header">
             <div className="flex-container">
-                <a href="/" className=""><h1 className="head-1">0nlyPets</h1></a>
+            <Link
+                to={`/${Auth.loggedIn() ? Auth.login: ""
+                }/signup/`} 
+                className="custom-a">
+                    <h1 className="head-1">OnlyPets</h1>
+            </Link>
                 <Nav
                 currentPage={currentPage}
                 setCurrentPage={setCurrentPage}
                 ></Nav>
             </div>
         </header>
-    );
+    )
 }
 
 export default Header;
+
+ 
