81e89796522ebe28e2ef7dfab6f7eecb43cf30f6
naomi <naomilounsbury@hotmail.com>
2022-04-14 13:38:49 -0700

    minor css changes

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -6,7 +6,7 @@ const CommentList = ({ comments }) => {
     <div className="card mb-3">
       <div className="card-header">
         <span className="text-start text-dark">Comments</span>
-        <span className="text-end">Likes</span>
+        <span className="text-end"></span>
       </div>
       <div className="card-body">
         {comments &&
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,34 +2,24 @@ import Nav from "../Nav";
 import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
 
-function Header({ currentPage, setCurrentPage }){
-    return(
-        <header className="top-header bg-info bg-opacity-25">
-            <div className="flex-container">
-            {Auth.loggedIn() ? (
-            <Link
-                to={`/`}
-                className="custom-a">
-                    <h1 className="head-1">OnlyPets</h1>
-            </Link>
-            ):(
-                <Link
-                    to={`/Signup`}
-                    className="custom-a">
-                    <h1 className="head-1">OnlyPets</h1>
-                </Link>)
-                
-            }
-           
-                <Nav
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-                ></Nav>
-            </div>
-        </header>
-    );
+function Header({ currentPage, setCurrentPage }) {
+  return (
+    <header className="top-header">
+      <div className="flex-container">
+        {Auth.loggedIn() ? (
+          <Link to={`/`} className="custom-a">
+            <h1 className="head-1">OnlyPets</h1>
+          </Link>
+        ) : (
+          <Link to={`/Signup`} className="custom-a">
+            <h1 className="head-1">OnlyPets</h1>
+          </Link>
+        )}
+
+        <Nav currentPage={currentPage} setCurrentPage={setCurrentPage}></Nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
-
- 
