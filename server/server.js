d3f196bb6599b4a83e02469ff95908f07a6e336c
amarten <alexdmarten@gmail.com>
2022-04-14 16:19:00 -0700

    add production environment check

--- server/server.js
+++ server/server.js
@@ -40,6 +40,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/static/images" ,express.static(path.join(__dirname, "./uploadedFiles")));
 
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
