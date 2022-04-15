ddce82d9f8010298708b91e40b74d4e4c6a1a780
amarten <alexdmarten@gmail.com>
2022-04-14 17:28:34 -0700

    fixed the uploadLink url in apollo

--- client/src/imageLink/index.js
+++ client/src/imageLink/index.js
@@ -1,2 +1,2 @@
-const linkText = "https://res.cloudinary.com/onlypets-images/image/upload/v1649975363/e3sekcrhfypz9gzslcjt.jpg";
+const linkText = "https://res.cloudinary.com/onlypets-images/image/upload/v1649982414/fdlt20ukdlvdybvtrrrs.jpg";
 module.exports = { linkText };
