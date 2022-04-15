1a625753af7e120a9c71fe41c15a618f85ef6a72
amarten <alexdmarten@gmail.com>
2022-04-14 17:13:15 -0700

    testing something for heroku

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const startServer = async () => {
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 startServer();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/static/images" ,express.static(path.join(__dirname, "./uploadedFiles")));
 
