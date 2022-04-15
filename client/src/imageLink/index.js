8158cf676aac143c78dff096029b645822373a7d
Chance <chancecrawford30@gmail.com>
2022-04-14 19:47:05 -0700

    delete image

--- client/src/imageLink/index.js
+++ client/src/imageLink/index.js
@@ -1,2 +1,2 @@
-const linkText = "https://res.cloudinary.com/dk1qjtwna/image/upload/v1649990479/srmekdyppk3mauwriztd.jpg";
+const linkText = "https://res.cloudinary.com/dk1qjtwna/image/upload/v1649990678/o0bly3esbp5qjlw6zf62.png";
 module.exports = { linkText };
